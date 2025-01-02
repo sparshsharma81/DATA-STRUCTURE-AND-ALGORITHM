a2a115f624be2c9841b14d875545d37e23659fdf
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-01-02 22:07:00 +0530

    sort

--- count_sort.java
+++ count_sort.java
@@ -1,4 +1,34 @@
 public class count_sort{
+    static int maxel(int[]arr){
+        int mx = Integer.MIN_VALUE;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i] > mx){
+                mx = arr[i];
+            }
+        }
+        return mx;
+    }
+    static void basiccount(int[] arr){
+        int mx = maxel(arr);
+        int[] count = new int[arr.length + 1];
+        for(int i = 0;i<arr.length-1;i++){
+            count[arr[i]]++;
+
+        }
+        int k = 0;
+        for(int i = 0;i<count.length;i++){
+            for(int j = 0;j<count[i]; j++){
+                arr[k++] = i;
+            }
+        }
+
+    }
+    static void display(int[] arr){
+        for(int i = 0;i<arr.length-1;i++){
+            System.out.print(arr[i]+" ");
+
+        }
+    }
     public static void main(String[] args) {
         //now we are doing the count sort...
         /*
@@ -10,5 +40,11 @@ public class count_sort{
          and then we will output the final array
          
          */
+        int[] arr = {1,6,3,4,1,2,1,4};
+        display(arr);
+        System.out.println(" ");
+        basiccount(arr);
+        display(arr);
+
     }
 }
