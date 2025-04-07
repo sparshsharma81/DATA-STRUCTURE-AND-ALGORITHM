512f58932883ce79bf2f51cebe5a5dc646fe191f
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-04-07 19:00:16 +0530

    sorting_a _string

---
+++ class/sorting1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sorting1 {
+    public static void main(String args[]){
+        String[] word = {"ab" , "aba" , "a"};
+        Arrays.sort(word);
+        for(String sh : word){
+            System.out.println(word);
+        }
+    }
+}
